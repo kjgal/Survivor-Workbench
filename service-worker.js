ee01375925d880a4d1013d2be4174e870de99e2e
kjgal <kaitlyn.mccaul@gmail.com>
2026-09-22 22:20:10 -0700

    Refresh cache for desktop Jerboa fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "survivor-workbench-v29";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./assets/icon-192.png", "./assets/icon-512.png", "./assets/ingredient-sprites.png", "./assets/ui-sprites.png", "./assets/survivor-emblem.png", "./assets/cooking-animation.png", "./assets/ambient-world.png", "./assets/stone-tablet.png", "./assets/jerboa-animation.png", "./assets/jerboa-final-full.png", "./assets/jerboa-final-hop.png", "./assets/jerboa-draped-rest.png", "./assets/goal-flask.png"];
+const CACHE = "survivor-workbench-v30";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./assets/icon-192.png", "./assets/icon-512.png", "./assets/ingredient-sprites.png", "./assets/ui-sprites.png", "./assets/survivor-emblem.png", "./assets/cooking-animation.png", "./assets/ambient-world.png", "./assets/stone-tablet.png", "./assets/jerboa-animation.png", "./assets/jerboa-final-full.png", "./assets/jerboa-top-compact.png", "./assets/jerboa-final-hop.png", "./assets/jerboa-draped-rest.png", "./assets/goal-flask.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
