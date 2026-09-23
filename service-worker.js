f6a8cdd3d4706d2b00b4472a7deb6eb84453b674
kjgal <kaitlyn.mccaul@gmail.com>
2026-09-22 21:54:23 -0700

    Publish Survivor Workbench v1

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "survivor-workbench-v28";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./assets/icon-192.png", "./assets/icon-512.png", "./assets/ingredient-sprites.png", "./assets/ui-sprites.png", "./assets/survivor-emblem.png", "./assets/cooking-animation.png", "./assets/ambient-world.png", "./assets/stone-tablet.png", "./assets/jerboa-animation.png", "./assets/jerboa-final-full.png", "./assets/jerboa-final-hop.png", "./assets/jerboa-draped-rest.png", "./assets/goal-flask.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
